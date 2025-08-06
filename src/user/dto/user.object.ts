c781fa8a49df43c03995cc10f9ec2c35b2c55b54
Tewodros Shimels <tewodrosshimels54@gmail.com>
2025-08-06 07:35:51 +0100

    update/changes

--- src/user/dto/user.object.ts
+++ src/user/dto/user.object.ts
@@ -2,6 +2,8 @@ import { ObjectType, Field, ID } from '@nestjs/graphql';
 import { Role } from '@prisma/client';
 
 @ObjectType('User')
+// This object type represents a user in the system, including their ID, email, role, active status, and timestamps for creation and updates.
+// It is used in GraphQL queries and mutations to interact with user data.
 export class User {
   @Field(() => ID)
   id: number;
